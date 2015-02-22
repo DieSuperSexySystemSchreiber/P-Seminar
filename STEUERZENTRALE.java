1086585305b1d9c08df425b7e0d95d8b33c83308
Kilian <steinberk@gmx.de>
2015-02-22 18:42:08 +0100

    Dokumentation von STEUERZENTRALE
    die Klasse Steuerezentrale wurde von Kilian dokumentiert

--- STEUERZENTRALE.java
+++ STEUERZENTRALE.java
@@ -2,7 +2,7 @@
  * Die Klasse STEUERZENTRALE ist die ausführende bzw. Anweisung gebende Klasse des Termgenerators.
  * 
  * @author (Kilian Steinberg)
- * @version (15/02/20)
+ * @version (15/02/22)
  */
 public class STEUERZENTRALE
 {
@@ -20,6 +20,14 @@ public class STEUERZENTRALE
         erster=erster.ErgebnisEinfuegen(erg);   
     }
 
+  
+    /**
+     * Method randTermKlasse
+     *
+     *Eine Unterklasse von TERM wird zufällig ausgewählt, mit welcher anschließend gearbeitet werden kann.
+     *
+     * @return eine der Klassen SUMME, DIFFERENZ, PRODUKT und DIVISION
+     */
     public TERM randTermKlasse()
     {
         TERM k;
@@ -48,6 +56,12 @@ public class STEUERZENTRALE
         }
     }
 
+    /**
+     * Method fRandTerm
+     * Erstellt einen zufällig generierten Term mit einem zufälligen Ergebnis und der Klammertiefe 2.
+     *
+     * @return Term
+     */
     public String fRandTerm()
     {
         TERM k=this.randTermKlasse();
@@ -58,6 +72,13 @@ public class STEUERZENTRALE
         return term ;
 
     }
+    /**
+     * Method ErgebnisPruefen
+     *Vergleicht das Ergebnis des Benutzers mit dem richtigen mit dem ältesten ungelösten Ergebnis
+     *
+     * @param erg Ergebnis des Benutzers
+     * @return eine Antwort: "Richtig" oder "Falsch" je nach dem ob das Ergebnis richtig war
+     */
     public String ErgebnisPruefen(double erg)
     {
         String antwort;
@@ -71,16 +92,37 @@ public class STEUERZENTRALE
         return antwort;
     }
 
+    /**
+     * Method showErster
+     *
+     * @return Das zuerst abgespeicherte Ergebnis.
+     */
     public ERGEBNIS showErster()
     {
         return erster;
     }
 
+    /**
+     * Method KlammerTerm
+     *Generiert einen zufälligen Term mit beliebiger Klammertiefe
+     *
+     * @param klammertiefe die Gewünschte Klammertiefe (z.B. 1 -> eine Zahl; 2 -> a+b)
+     * @return Term
+     */
     public  String KlammerTerm(int klammertiefe)
     {
         return TermKlammertiefe(klammertiefe,0,true);
     }
 
+    /**
+     * Method TermKlammertiefe
+     * Erzeugt einen Term mit gewünschter Klammertiefe und wahlweise für ein gewünschtes Ergebnis.
+     *
+     * @param klammertiefe die gewünschte Klammertiefe
+     * @param erg ein Ergebnis welches Rekursiv in einen Term zerlegt werden kann
+     * @param start gibt an ob dies der fertige Term oder nur ein Zwischenterm dessen ist
+     * @return Term
+     */
     public String TermKlammertiefe(int klammertiefe, double erg, boolean start)
     {
         TERM b=this.randTermKlasse();
@@ -88,11 +130,14 @@ public class STEUERZENTRALE
         RANDOM f=new RANDOM();
         double lösung=f.getErgebnis();
         if (start==false)
+        // Wenn start = false ist, dann wird der parameter erg in einen weiteren Term aufgespalten
         {lösung=erg;
             term =b.TermKlammertiefe(klammertiefe,lösung);
             return term ;
         }
         else
+       //Wenn start = true ist, dann wird das Ergebnis abgespeichert.
+       //Somit wird sichergestellt, dass nur das Endergebnis abgespeichert wird.
         {
             term =b.TermKlammertiefe(klammertiefe,lösung);
             this.ErgebnisEinfuegen(b.Ergebnis());
@@ -102,7 +147,13 @@ public class STEUERZENTRALE
         
     }
 
-    public char[] gibEins()
+    
+   /** Method gibEins
+    * Testmethode; macht aus einem String ein char array.
+    * 
+    * @return char array
+    */
+   public char[] gibEins()
     {
         String hello = "Hello";
         char[] chars = KlammerTerm(3).toCharArray();
