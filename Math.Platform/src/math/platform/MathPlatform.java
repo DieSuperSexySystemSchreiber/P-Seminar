f20e0374a2d1d37a20d48cd5c037ac43c3c8548a
DavidMueller1 <david.muelle2@web.de>
2015-02-22 17:34:48 +0100

    Logo hinzugefügt

---
+++ Math.Platform/src/math/platform/MathPlatform.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.platform;
+
+/**
+ *
+ * @author David Müller
+ */
+public class MathPlatform {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI window = new GUI();
+        window.setVisible(true);
+    }
+    
+}
