f91eb000dd5be572806f0061608c1b4be43ecbff
IngolfSchmitt <ingolf@schmittrausch.de>
2015-03-18 12:09:54 +0100

    GUI als BluJ-Projekt hinzugefuegt

---
+++ GUI/MathPlatform.java
@@ -0,0 +1,11 @@
+public class MathPlatform {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI2 window = new GUI2();
+        window.setVisible(true);
+    }
+    
+}
