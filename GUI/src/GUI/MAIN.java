5aa790d276e1a44fc7d42db708d94332019c4e08
DavidMueller1 <david.muelle2@web.de>
2015-04-08 16:03:56 +0200

    GUI Update: Menüleiste
    Die GUI besitzt jetzt eine Grundstruktur mit Menüleiste, worüber man die
    Aufgabeneinstellungen erreicht. wip

---
+++ GUI/src/GUI/MAIN.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author David Müller
+ */
+public class MAIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MAINGUI window = new MAINGUI();
+        window.setVisible(true);
+    }
+    
+}
