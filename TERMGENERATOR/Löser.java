ad058891d74e39228bf86be60d5bee9e9b8620ac
HansTofu <momoxd1998@gmail.com>
2015-06-11 18:26:26 +0200

    Löser Klasse hinzugefügt
    Klasse zum entfernen unnötiger Klammern inklusive,
    Löser hat einen winzigen, sehr nervigen bug,
    Klammerentferner entfernt bisher nur die Klammern direkt um die zahlen.
    Um die Dokumentation muss ich mich auch noch kümmern

---
+++ TERMGENERATOR/Löser.java
@@ -0,0 +1,152 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Löser.
+ * 
+ * @author Morgan, Leon 
+ * @version 11.06.2015
+ */
+import java.util.regex.*;
+public class Löser
+{
+    public Löser()
+    {
+    }
+
+    public double lösen(String term)
+    {
+
+        // das einzige problem: die vergleiche bei den if(c2=(+|-|*|/)
+        //sonst perfekt funktionstüchtig
+        //außer vorvorletzte Zeile vlt -1 statt -2 wenn es spackt
+
+        String a = "\\(\\-?\\d+\\).\\(\\-?\\d+\\)";
+        String f = "";
+        Pattern pattern = Pattern.compile(a);
+        Matcher matcher = pattern.matcher(term);
+        matcher.find();
+        
+        while (matcher.find())
+        {
+            matcher = pattern.matcher(term);
+
+            matcher.find();
+
+            System.out.println(matcher.group());
+
+            String b = term;
+            String b1 = b.substring(0,matcher.start()); //take the part of the string whitch is not changed
+            String b2 = b.substring(matcher.end());
+
+            String c = matcher.group();
+            String[] parts = c.split("\\(|\\)");
+            String c1 = parts[1];
+            String c2 = parts[2];
+            String c3 = parts[3];
+            double d1 = Integer.parseInt(c1);
+            double d3 = Integer.parseInt(c3);
+            String lol = "+";
+            Pattern mattern = Pattern.compile(lol);
+            Matcher patcher = mattern.matcher(c2);
+           if(patcher.matches()==true)
+           {
+                double e = (d1) + (d3);
+                f = e + "";
+            }
+            else if(c2=="-")
+            {
+                double e = (d1) - (d3);
+                f = e + "";
+            }
+            else if(c2 == "*")
+            {
+                double e = (d1) * (d3);
+                f = e + "";
+            }
+            else if(c2 == "/")
+            {
+                double e = (d1) / (d3);
+                f = e + "";
+            }
+            else
+            {
+                System.out.println("error");
+                f= "";
+                break;
+            } 
+            term= b1 + f + b2;
+        }
+        String i = term.substring(1,term.length()-2);
+        double yay = Integer.parseInt(i);
+
+        return yay;
+    }
+
+    public String entklammern(String term)
+    {
+        String a =  "\\(\\d+(\\.\\d+)?\\)"; // "[^\\+\\-\\*\\/]\\(\\-?\\d+(\\.\\d+)?)\\)|.\\(\\d+(\\.\\d+)?\\)";
+        Pattern pattern = Pattern.compile(a);
+        Matcher matcher = pattern.matcher(term);
+        while (matcher.find())
+        {
+
+            matcher = pattern.matcher(term);   //set the new term
+
+            matcher.find();   //search the pattern in the new term
+
+            System.out.println(matcher.group());
+
+            String b = term;
+            String teil1 = b.substring(0,matcher.start()); //take the part of the string whitch is not changed
+            String teil2 = b.substring(matcher.end());
+
+            String string = matcher.group();
+            String[] parts = string.split("\\(|\\)");
+            term = teil1 + parts[1] + teil2;
+            
+           /** String ganzerString = matcher.group();
+            String teilString1 = ganzerString .substring(0,1);  // delete the (
+            String teilString2 = ganzerString .substring(1);  
+
+            String string = teilString2;
+            String[] parts = string.split("\\)"); // delete the )
+            String part1 = parts[0]; 
+            
+
+            term= teil1 + teilString + part1 + teil2; // this put all together again */
+        }
+
+        /**          // pattern muss noch erstellt werden u.a
+
+        a = "([^\\-\\*\\/]\\(\\d+.+(\\(\\-\\d+\\))|(\\d+)\\)[^\\*\\/])|([^\\+\\-\\/]\\(\\d+[\\*\\/](\\(\\-\\d+\\))|(\\d+)\\).)";
+        pattern = Pattern.compile(a);
+        matcher = pattern.matcher(term);
+
+        while(matcher.find())
+        {
+
+        matcher = pattern.matcher(term);    //set the new term
+
+        matcher.find();     //search the pattern in the new term
+
+        System.out.println(matcher.group());
+
+        String b = term;
+        String teil1 = b.substring(0,matcher.start()); //take the part of the string
+        //which is not changed
+        String teil2 = b.substring(matcher.end());
+
+        String ganzerString = matcher.group();
+        String teilString1 = ganzerString .substring(0,1);  // delete the (
+        String teilString2 = ganzerString .substring(2);  
+
+        String string = teilString2;
+        String parts = string.substring(0,string.length()-1);     //delete the )
+
+        term=teil1+teilString1+parts+teil2;
+
+        }
+         */
+        return term;
+
+    }
+}
