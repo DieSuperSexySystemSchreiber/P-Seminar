fb2394ff0cfa93a6b6335ccd25c805c3007ef20f
HansTofu <momoxd1998@gmail.com>
2015-10-21 16:30:30 +0200

    massentester programmiert
    ist im löser in den alten dokumenten
    er überprüft große mengen an generierten termen auf ihre richtigkeit.ich
    versuche mal ihn zum projekt dazuzuziehen.

--- Alte Dokumente/P-Seminar(vollst)/Löser.java
+++ Alte Dokumente/P-Seminar(vollst)/Löser.java
@@ -2,23 +2,39 @@
  * Beschreiben Sie hier die Klasse Löser.
  * 
  * @author Morgan, Leon 
- * @version 11.06.2015
+ * @version 21.10.2015
  */
 import java.util.regex.*;
 public class Löser
 {
+    TERM Fehlerhaft[] = new TERM[0];
     public Löser()
     {
+          
     }
 
     public double lösen(String term)
     {
-        boolean t = Prüfer(term);
+        boolean t = SyntaxPrüfer(term);
         if(t == false)
-        return -0.000000000000000000000000000000001;
+            return -0.000000000000000000000000000000001;
+        String a =  "\\(\\-?\\d+(\\.\\d+)?\\)"; 
+        Pattern pattern = Pattern.compile(a);
+        Matcher matcher = pattern.matcher(term);
+        while (matcher.find())
+        {
+
+            matcher = pattern.matcher(term);   //set the new term
+
+            matcher.find();   //search the pattern in the new term
+
+            System.out.println(matcher.group());
+
+            term = term.substring(0,matcher.start())+term.substring(matcher.start()+1,matcher.end()-1)+term.substring(matcher.end());
+        }
         term = "(" + term + ")";
         int u =1;
-        
+
         String k ="\\d\\)*[\\+|\\-|\\*|\\/]";
         Pattern mattern = Pattern.compile(k);
         Matcher patcher = mattern.matcher(term);
@@ -33,7 +49,7 @@ public class Löser
             term = k1 + "l)" + k2 + "(" + k3;
             u++;
         }
-        
+
         String g = term;
         String[] part = g.split("l");
         String g1 = "";
@@ -42,13 +58,11 @@ public class Löser
             g1 = g1 + part[n];
         }
         term = g1;
-        String a = "\\(\\-?\\d+\\.?\\d*\\).\\(\\-?\\d+\\.?\\d*\\)";
+        a = "\\(\\-?\\d+\\.?\\d*\\).\\(\\-?\\d+\\.?\\d*\\)";
         String f = "";
-        Pattern pattern = Pattern.compile(a);
-        Matcher matcher = pattern.matcher(term);
-        matcher.find();
-        
-        while (matcher.find())
+        pattern = Pattern.compile(a);
+        matcher = pattern.matcher(term);
+        while(matcher.find())
         {
             matcher = pattern.matcher(term);
 
@@ -71,8 +85,8 @@ public class Löser
             String y = "-";
             String w = "*";
             String s = "/";
-           if(z.equals(c2))
-           {
+            if(z.equals(c2))
+            {
                 double e = (d1) + (d3);
                 f = e + "";
             }
@@ -103,7 +117,7 @@ public class Löser
         String yay = term.substring(1,term.length()-1);
         double awsome = Double.parseDouble(yay);
         return awsome;
-    
+
     }
 
     public String entklammern(String term)
@@ -120,59 +134,88 @@ public class Löser
 
             System.out.println(matcher.group());
 
-            String b = term;
-            String teil1 = b.substring(0,matcher.start()); //take the part of the string whitch is not changed
-            String teil2 = b.substring(matcher.end());
-
-            String string = matcher.group();
-            String[] parts = string.split("\\(|\\)");
-            term = teil1 + parts[1] + teil2; //this puts it all together again
-            
-
-
-
-           /** String ganzerString = matcher.group();
-            String teilString1 = ganzerString .substring(0,1);  // delete the (
-            String teilString2 = ganzerString .substring(1);  
-            String string = teilString2;
-            String[] parts = string.split("\\)"); // delete the )
-            String part1 = parts[0]; 
-            
-            term= teil1 + teilString + part1 + teil2; // this put all together again */
-
+            term = term.substring(0,matcher.start())+term.substring(matcher.start()+1,matcher.end()-1)+term.substring(matcher.end());
         }
-
-        /**          // pattern muss noch erstellt werden u.a
-        a = "([^\\-\\*\\/]\\(\\d+.+(\\(\\-\\d+\\))|(\\d+)\\)[^\\*\\/])|([^\\+\\-\\/]\\(\\d+[\\*\\/](\\(\\-\\d+\\))|(\\d+)\\).)";
+        // pattern muss noch erstellt werden u.a
+        a = "\\+\\(+\\-?\\d+(\\.\\d\\d)?[\\+|\\-|\\*|\\/]\\-?\\d+(\\.\\d\\d)?\\)+[\\+\\-]|[\\+|\\*|\\/]\\(+\\-?d+(\\.\\d\\d)?[\\*|\\/]\\-?\\d+(\\.\\d\\d)?\\)+[\\+|\\-|\\*|\\/]";
+        // +(Z+/-/*//Z)+/-                                                               +/*//(Z*//Z)+/-/*//
+        term = "+"+term+"+";
         pattern = Pattern.compile(a);
         matcher = pattern.matcher(term);
         while(matcher.find())
         {
-        matcher = pattern.matcher(term);    //set the new term
-        matcher.find();     //search the pattern in the new term
-        System.out.println(matcher.group());
-        String b = term;
-        String teil1 = b.substring(0,matcher.start()); //take the part of the string
-        //which is not changed
-        String teil2 = b.substring(matcher.end());
-        String ganzerString = matcher.group();
-        String teilString1 = ganzerString .substring(0,1);  // delete the (
-        String teilString2 = ganzerString .substring(2);  
-        String string = teilString2;
-        String parts = string.substring(0,string.length()-1);     //delete the )
-        term=teil1+teilString1+parts+teil2;
+            matcher = pattern.matcher(term);    //set the new term
+            matcher.find();     //search the pattern in the new term
+            System.out.println(matcher.group());
+            String b = term;
+            String teil1 = b.substring(0,matcher.start()); //take the part of the string
+            //which is not changed
+            String teil2 = b.substring(matcher.end());
+            String teilString1 = term.substring(matcher.start(),matcher.start()+1);  // delete the (
+            String teilString2 = term.substring(matcher.start()+2,matcher.end()-2);
+            String teilString3 = term.substring(matcher.end()-1,matcher.end());
+            term=teil1+teilString1+teilString2+teilString3+teil2;
+            matcher = pattern.matcher(term);
         }
-         */
+        term = term.substring(1,term.length()-1);
+
         return term;
 
     }
-    public boolean Prüfer(String l)
+
+    public boolean SyntaxPrüfer(String l)
     {
-        String prüfer="(\\(*\\-?\\d+(\\.\\d\\d)?\\)*[\\*|\\/|\\+|\\-])+\\(*\\-?\\d+(\\.\\d\\d)?\\)*";
+        String prüfer="(\\(*\\-?\\d+(\\.\\d+)?\\)*[\\*|\\/|\\+|\\-])+\\(*\\-?\\d+(\\.\\d+)?\\)*";
         Pattern muster = Pattern.compile(prüfer);
         Matcher übereinstimmer = muster.matcher(l);
         if(übereinstimmer.matches())
-        return true;    
+            return true;    
         else return false;
     }
+
+    public boolean massentester(int anzahl,boolean aoAddition,boolean aoSubstraction,boolean aoMultiplication, boolean aoDivision,int bracketDepth, int substitutions, int digits, boolean oPositive, boolean wFraction)
+    {
+        boolean geht = true;
+        String r;
+        TERM Fehlerhatig[];
+        int zähler=0;
+        for(int y=0;y<anzahl;y++)
+        {
+            TERM h =new TERM(aoAddition, aoSubstraction, aoMultiplication, aoDivision, bracketDepth, substitutions, digits, oPositive, wFraction);
+            String c = h.result+"="+h.wurzel.infix();
+            String[] parts = c.split("\\=");
+            String e = parts[0];
+            double f = Double.parseDouble(e);
+            String t = parts[1];
+            
+            if(f != this.lösen(t))
+            {
+                if(zähler==0)
+            {
+                Fehlerhaft = new TERM[1];
+                Fehlerhaft[0] = h;
+                zähler++;
+                geht =false;
+            }
+            else
+            {
+                
+                TERM Fehlerhaftig[] = new TERM[zähler];
+                for(int x=0;x<zähler;x++)
+                {
+                    Fehlerhaftig[x] = Fehlerhaft[x];
+                }
+                Fehlerhaft = new TERM[zähler+1];
+                for(int x=0;x<zähler;x++)
+                {
+                    Fehlerhaft[x] = Fehlerhaftig[x];
+                }
+                Fehlerhaft[zähler]=h;
+                zähler++;
+            }
+        }
+        }
+        return geht;
+    }
+
 }
